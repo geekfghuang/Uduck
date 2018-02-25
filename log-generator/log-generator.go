41a75f13b2bc2a14e7a6a93f11424e77d86da73e
geekfghuang <geek_huangfugui@163.com>
2018-02-25 14:38:34 +0800

    加入定时调度

--- log-generator/log-generator.go
+++ log-generator/log-generator.go
@@ -1,3 +1,4 @@
+// nohup ./log-generator ../conf/gen.conf &
 package main
 
 import (
@@ -9,6 +10,7 @@ import (
 	"time"
 	"math/rand"
 	"strconv"
+	"github.com/robfig/cron"
 )
 
 type Conf struct {
@@ -20,15 +22,17 @@ type Conf struct {
 	KeywordsInitLength int `xml:"keywordsInitLength"`
 	LogFile string `xml:"logFile"`
 	LogNumOnce int `xml:"logNumOnce"`
+	Period int `xml:"period"`
 }
 
 var (
+	conf Conf
 	ips []string
 	keywords []string
 	action = []string{"/search?keyword=", "/item/", "/store/", "/pay/"}
 )
 
-func initIps(conf *Conf) {
+func initIps() {
 	ips = make([]string, 0, conf.IpsInitLength)
 	content, err := ioutil.ReadFile(conf.IpFilePath)
 	if err != nil {
@@ -43,7 +47,7 @@ func initIps(conf *Conf) {
 	}
 }
 
-func initKeywords(conf *Conf) {
+func initKeywords() {
 	keywords = make([]string, 0, conf.KeywordsInitLength)
 	content, err := ioutil.ReadFile(conf.KeywordFilePath)
 	if err != nil {
@@ -58,7 +62,7 @@ func initKeywords(conf *Conf) {
 	}
 }
 
-func logItem(conf *Conf) []byte {
+func logItem() []byte {
 	random := rand.New(rand.NewSource(time.Now().UnixNano()))
 	userId := strconv.FormatInt(int64(random.Intn(conf.MaxUserId)), 10)
 	ip := ips[random.Intn(len(ips))]
@@ -76,16 +80,23 @@ func logItem(conf *Conf) []byte {
 	return []byte(userId + "\t" + ip + "\t" + time + "\t" + actionUrl + "\n")
 }
 
-func generateLogs(conf *Conf) {
+func generateLogs() {
 	fd, err := os.OpenFile(conf.LogFile, os.O_CREATE | os.O_RDWR | os.O_APPEND, 0666)
 	if err != nil {
 		fmt.Println("error open " + conf.LogFile + ": %v\n", err)
 		os.Exit(1)
 	}
 	for i := 0; i < conf.LogNumOnce; i++ {
-		fd.Write([]byte(logItem(conf)))
+		fd.Write([]byte(logItem()))
 	}
 	fd.Close()
+}
+
+func crontab() {
+	period := "*/" + strconv.FormatInt(int64(conf.Period), 10) + " * * * * *"
+	c := cron.New()
+	c.AddFunc(period, generateLogs)
+	c.Start()
 }
 
 func main() {
@@ -98,14 +109,14 @@ func main() {
 		fmt.Printf("error read gen.conf: %v\n", err)
 		os.Exit(1)
 	}
-	var conf Conf
 	err = xml.Unmarshal(content, &conf)
 	if err != nil {
 		fmt.Printf("error parse gen.conf: %v\n", err)
 		os.Exit(1)
 	}
 
-	initIps(&conf)
-	initKeywords(&conf)
-	generateLogs(&conf)
+	initIps()
+	initKeywords()
+	crontab()
+	select {}
 }
