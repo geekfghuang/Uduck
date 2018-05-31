178d3852edcbb81c592a01d2cfcbe67501014ae2
geekfghuang <geek_huangfugui@163.com>
2018-05-31 17:52:33 +0800

    update spark streaming direct方式对接kafka

--- spark-calculator/src/main/scala/Calculator.scala
+++ spark-calculator/src/main/scala/Calculator.scala
@@ -1,24 +1,42 @@
+import kafka.serializer.StringDecoder
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 /**
   * 作业启动命令：
-  * ./spark-submit --master local[5] --class Calculator --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.0 ../test-lib/spark-calculator-1.0-SNAPSHOT.jar localhost:2181 geekfghuang Uduck1688 1
+  * ./spark-submit --master local[5] --class Calculator --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.0 ../test-lib/spark-calculator-1.0-SNAPSHOT.jar localhost:2181 geekfghuang UduckRec 1
+  * ./spark-submit --master local[5] --class Calculator --packages org.apache.spark:spark-streaming-kka-0-8_2.11:2.2.0 ../test-lib/spark-calculator-direct-1.0-SNAPSHOT.jar localhost:9093,localhost:9094 UduckRec
   */
 object Calculator {
   def main(args: Array[String]): Unit = {
-    if (args.length != 4) {
-      println("Usage: Calculator zk groupId topics numThreads")
+//    if (args.length != 4) {
+//      println("Usage: Calculator zk groupId topics numThreads")
+//      System.exit(1)
+//    }
+//
+//    val Array(zk, groupId, topics, numThreads) = args
+//    val sparkConf = new SparkConf().setAppName("Calculator")//.setMaster("local[5]")
+//    val ssc = new StreamingContext(sparkConf, Seconds(30))
+//
+//    val topicsMap = topics.split(",").map((_, numThreads.toInt)).toMap
+//    val messages = KafkaUtils.createStream(ssc, zk, groupId, topicsMap)
+
+    if(args.length != 2) {
+      System.err.println("Usage: Calculator <brokers> <topics>")
       System.exit(1)
     }
 
-    val Array(zk, groupId, topics, numThreads) = args
-    val sparkConf = new SparkConf().setAppName("Calculator")//.setMaster("local[5]")
+    val Array(brokers, topics) = args
+    val sparkConf = new SparkConf().setAppName("Calculator")
+      //.setMaster("local[5]")
     val ssc = new StreamingContext(sparkConf, Seconds(30))
+    val topicsSet = topics.split(",").toSet
+    val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
 
-    val topicsMap = topics.split(",").map((_, numThreads.toInt)).toMap
-    val messages = KafkaUtils.createStream(ssc, zk, groupId, topicsMap)
+    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
+      ssc, kafkaParams, topicsSet
+    )
 
     // 获得日志数据
     val logs = messages.map(_._2)
